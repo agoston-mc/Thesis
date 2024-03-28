103715771b6d4f5d16fa4de3b56ffff9be6b12b0
agoston.czobor <agoston.czobor@aimotive.com>
2024-03-28 10:40:18 +0100

    cts test generation redo

--- generate_cts_test_cases.py
+++ generate_cts_test_cases.py
@@ -7,13 +7,13 @@ usage:
     `python generate_test_scrit_cases.py`
     it will generate generated_tests.py with test cases in current wd
         from ./outputs with default tolerances of 1e-5
-        
+
 '''
 
 script = """
 @tvm.testing.parametrize_targets
-def test_{case}(target, dev):
-    verify_model('{case}', inputs, outputs, target, dev, rtol, atol)
+def test_cts_{case}(target, dev):
+    verify_model('{case_path}', target, dev, rtol=1e-5, atol=1e-5)
 """
 
 pwd = os.getcwd()
@@ -22,7 +22,7 @@ folders = os.listdir(graphs_dir)
 
 with open(os.path.join(pwd, 'generated_tests.py'), 'w+') as f:
     for case in folders:
-        case_path = os.path.join(graphs_dir, case)
+        case_path = case
         if '-' in case:
             case = case.replace('-', '_')
         print(script.format(case=case, case_path=case_path), file=f)
